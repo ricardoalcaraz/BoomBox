6f551922ad59b770669eaf2e1f3079ba0ae4ec09
ricardoalcaraz <ralcarazerhs@gmail.com>
2018-04-12 14:56:27 -0700

    Added more documentation to functions

--- BoomBox/main/CountdownTimer.h
+++ BoomBox/main/CountdownTimer.h
@@ -9,21 +9,44 @@
 
 class CountdownTimer:public Subject {
 	public:
+		/**Constructor*/
 		CountdownTimer();
+
+		/**Return the current time in seconds*/
 		uint16_t getCurrentTime();
+
+		/**Add seconds to the current time*/
 		void addSeconds( uint8_t seconds );
+
+		/**Reset the countdown and start from the beginning*/
 		void resetCountdown();
+
+		/**Stop the countdown
+		 * Will stop the interrupt timer from counting down
+		 * and will free up the resources*/
 		void stopCountdown();
-    void startCountdown();
+
+		/**Start the countdown
+		 * Will start the countdown again if it was stopped
+		 * Other wise will do nothing*/
+		void startCountdown();
+
+		/**Start the countdown from a specified time
+		 * Will start counting down in seconds*/
 		void startCountdown( uint16_t initialTime );
-    void countdown();
-    static volatile int16_t secondsLeft;
-    static Subject* getSubject();
-    
+
+		/**Decrement the seconds counter*/
+		void countdown();
+
+		static volatile int16_t secondsLeft;
+
+		/**Return the internal subject so obervers can get registered*/
+		static Subject* getSubject();
+		
 	private:
 		static int16_t initialTime;
 		IntervalTimer clockTimer;
-    static Subject subj;
+		static Subject subj;
 };
 
 #endif
--- BoomBox/main/Observer.cpp
+++ BoomBox/main/Observer.cpp
@@ -1,6 +1,7 @@
 #include "Observer.h"
 #include "Subject.h"
 
+/**Register the observer to a subject*/
 void Observer::attachSubject( Subject *subject ) {
 	subject->registerObserver( this );
 }
