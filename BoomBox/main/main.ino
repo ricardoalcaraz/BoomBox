b0f085896f6a2618f3fa8aec69f56eb6efb485a2
wellfuck-Kris10 <kristenoduca@gmail.com>
2018-05-11 18:12:16 -0700

    Timer Fixed

--- BoomBox/main/main.ino
+++ BoomBox/main/main.ino
@@ -103,5 +103,5 @@ void one_sec_timer_init() {
 
 ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
 //generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
-  countdown.countdown();
+  countdownClock.countdown();
 }
