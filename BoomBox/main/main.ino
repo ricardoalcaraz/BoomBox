2fff42f77250ca5d4ba79f889c871dd0d119eb4c
ricardoalcaraz <ralcarazerhs@gmail.com>
2018-05-11 18:01:42 -0700

    Added timer to arduino uno

--- BoomBox/main/main.ino
+++ BoomBox/main/main.ino
@@ -58,7 +58,7 @@ void setup() {
   
   //Create clock that countdown every second
   countdownClock.startCountdown( 360 );
-  
+  one_sec_timer_init();
   //Update any observers that may need this info
   clockDisplay.attachSubject( countdownClock.getSubject() );
   
@@ -86,4 +86,17 @@ void loop() {
 
 }
 
+void one_sec_timer_init() {
+    TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+}
 
